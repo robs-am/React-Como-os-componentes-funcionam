3efa38ed5b853645330c691bfaef0532a5c9c2d5
robs-am <roberta.amaro89@gmail.com>
2022-12-27 15:47:55 -0300

    aumenta o tamanho do icone

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -3,7 +3,7 @@ import './colaborador.css'
 
 const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
     return (<div className="colaborador">
-        <div className='deletar' onClick={aoDeletar}>deletar</div>
+        <AiFillCloseCircle size={25} className='deletar' onClick={aoDeletar} />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
